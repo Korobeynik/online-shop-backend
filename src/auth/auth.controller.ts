a9716d3a8ab6944f024a434fe0a03e10f0fa5ef5
korobeynik <korobeynik77@gmail.com>
2025-05-21 10:49:35 +0300

    Working with login/logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   Body,
   Controller,
@@ -5,24 +7,59 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
+  Res,
+  UnauthorizedException,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { Response, Request } from 'express';
+import { ref } from 'process';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
-  async login(@Body() dto: AuthDto) {
-    return this.authService.login(dto);
+  async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.login(dto);
+    this.authService.addRefreshTokenResponse(res, refreshToken);
+    return response;
   }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('register')
-  async register(@Body() dto: AuthDto) {
-    return this.authService.register(dto);
+  async register(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.register(dto);
+    this.authService.addRefreshTokenResponse(res, refreshToken);
+    return response;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getNewTokens(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME];
+
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenFromResponse(res);
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+    const { refreshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    );
+
+    this.authService.addRefreshTokenResponse(res, refreshToken);
+    return response;
   }
 }
